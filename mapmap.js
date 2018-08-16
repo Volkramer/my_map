7d13fa660ab6ab13a96b6b76b92cc084af639222
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-08-16 15:11:14 +0200

    add button center on me + code

---
+++ mapmap.js
@@ -0,0 +1,26 @@
+var mymap;
+var myposition;
+
+window.onload = function () {
+    init();
+
+    $("#locate").on("click", function () {
+        findMe();
+    });
+}
+
+function init() {
+    mymap = L.map('mapmap');
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(mymap);
+    mymap.locate({ setView: true, zoom: 10 });
+}
+
+
+function findMe() {
+    mymap.locate({ setView: false });
+    mymap.on("locationfound", function (e) {
+        myposition = e.latlng;
+    });
+    console.log(myposition);
+    mymap.panTo(myposition);
+}
