7f1149d37d5eadc03c5ef05ab16241cf14521f83
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-08-17 16:08:36 +0200

    mapquest version

--- mapmap.js
+++ mapmap.js
@@ -1,26 +1,27 @@
 var mymap;
 var myposition;
+L.mapquest.key = 'NeNAJsQv8JMJX1LGXiPKgRfC6iHyuGBT';
 
 window.onload = function () {
-    init();
+  init();
 
-    $("#locate").on("click", function () {
-        findMe();
-    });
+  $("#locate").on("click", function () {
+    findMe();
+  });
 }
 
 function init() {
-    mymap = L.map('mapmap');
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(mymap);
-    mymap.locate({ setView: true, zoom: 10 });
+  mymap = L.mapquest.map('map', {
+    center: [37.7749, -122.4194],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
 }
 
-
 function findMe() {
-    mymap.locate({ setView: false });
-    mymap.on("locationfound", function (e) {
-        myposition = e.latlng;
-    });
-    console.log(myposition);
-    mymap.panTo(myposition);
+  mymap.locate({ setView: true });
+  mymap.on("locationfound", function (e) {
+    myposition = e.latlng;
+  });
+  console.log(myposition);
 }
