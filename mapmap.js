887be0de3dcaea11fb51662eb453d11595a03ec3
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-08-18 20:34:32 +0200

    geolocalisation + marker

--- mapmap.js
+++ mapmap.js
@@ -1,20 +1,28 @@
 var mymap;
 var myposition;
-L.mapquest.key = 'NeNAJsQv8JMJX1LGXiPKgRfC6iHyuGBT';
+var key = 'NeNAJsQv8JMJX1LGXiPKgRfC6iHyuGBT';
+
 
 window.onload = function () {
   init();
 
-  $("#locate").on("click", function () {
+  $("#locate").on("click", function() {
     findMe();
+  });
+
+  $("#geoposition").on("change", function(){
+    var value = $("#geoposition").val();
+    console.log(value);
+    geoposition(value);
   });
 }
 
 function init() {
+  L.mapquest.key = key;
   mymap = L.mapquest.map('map', {
-    center: [37.7749, -122.4194],
+    center: [48.58, 7.76],
     layers: L.mapquest.tileLayer('map'),
-    zoom: 12
+    zoom: 10
   });
 }
 
@@ -24,4 +32,26 @@ function findMe() {
     myposition = e.latlng;
   });
   console.log(myposition);
+}
+
+function geoposition(value){
+  value = value.replace(/ /g, "+");
+  var dataSend = {
+    "location": value,
+    "option": [{
+      "thumbMaps": false
+    }],
+  };
+
+  $.post('http://www.mapquestapi.com/geocoding/v1/address?key=' + key, dataSend, function(data) {
+    for (var result of data.results[0].locations) {
+      console.log(result);
+      marker(result);
+    }
+  });
+}
+
+function marker(data){
+  var marker = L.marker(data.latLng, {autoPan: true}).addTo(mymap);
+  marker.bindPopup('<p>'+data.adminArea5+'<br/>'+data.latLng.lat+', '+data.latLng.lng+'</p>').openPopup();
 }
