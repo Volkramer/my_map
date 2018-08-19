c2e08eac182f67624c9353dc0c9b7e8b5fe2e120
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-08-19 17:19:25 +0200

    add function routing (data send)

--- mapmap.js
+++ mapmap.js
@@ -1,7 +1,9 @@
 var mymap;
-var myposition;
 var key = 'NeNAJsQv8JMJX1LGXiPKgRfC6iHyuGBT';
 
+/*
+**  onLoad function, detect when button or form is used
+*/
 
 window.onload = function () {
   init();
@@ -10,12 +12,27 @@ window.onload = function () {
     findMe();
   });
 
-  $("#geoposition").on("change", function(){
-    var value = $("#geoposition").val();
-    console.log(value);
+  $("#geoSearch").on("change", function(){
+    var value = $("#geoSearch").val();
     geoposition(value);
   });
+
+  $("#geoStart").on("change", function(){
+    if ($("#geoStart").val() != "" && $("#geoEnd").val() != "") {
+      routing($("#geoStart").val(), $("#geoEnd").val());
+    }
+  });
+
+  $("#geoEnd").on("change", function(){
+    if ($("#geoStart").val() != "" && $("#geoEnd").val() != ""){
+      routing($("#geoStart").val(), $("#geoEnd").val());
+    }
+  });
 }
+
+/*
+**  Map initialisation
+*/
 
 function init() {
   L.mapquest.key = key;
@@ -26,6 +43,10 @@ function init() {
   });
 }
 
+/*
+**  Geolocalisation of the user
+*/
+
 function findMe() {
   mymap.locate({ setView: true });
   mymap.on("locationfound", function (e) {
@@ -34,15 +55,18 @@ function findMe() {
   console.log(myposition);
 }
 
+/*
+**  place search via remote server from mapquestapi
+*/
+
 function geoposition(value){
-  value = value.replace(/ /g, "+");
   var dataSend = {
     "location": value,
-    "option": [{
+    "options": {
       "thumbMaps": false
-    }],
+    },
   };
-
+  console.log(dataSend);
   $.post('http://www.mapquestapi.com/geocoding/v1/address?key=' + key, dataSend, function(data) {
     for (var result of data.results[0].locations) {
       console.log(result);
@@ -51,7 +75,36 @@ function geoposition(value){
   });
 }
 
+/*
+**  Place a marker at given coordinate
+*/
+
 function marker(data){
   var marker = L.marker(data.latLng, {autoPan: true}).addTo(mymap);
   marker.bindPopup('<p>'+data.adminArea5+'<br/>'+data.latLng.lat+', '+data.latLng.lng+'</p>').openPopup();
+}
+
+function routing(start, end){
+  console.log(start);
+  console.log(end);
+  var dataSend = {
+    "locations": [
+      start,
+      end
+    ],
+    "options": {
+        "locale": "fr_FR",
+        "unit": "k",
+    }
+  };
+  console.log(dataSend);
+  $.ajax({
+    type: 'POST',
+    url: 'http://www.mapquestapi.com/directions/v2/route?key=' + key,
+    data: JSON.stringify( dataSend ),
+    contentType: "application/json",
+    dataType: 'json'
+  }).done(function(data) {
+    console.log(data);
+  });
 }
