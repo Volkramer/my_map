40fa442accb3ba69bdb6a332344db38ff8fbcf1a
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-08-22 12:03:39 +0200

    add function flag shape narrative

--- mapmap.js
+++ mapmap.js
@@ -28,6 +28,9 @@ window.onload = function () {
       routing($("#geoStart").val(), $("#geoEnd").val());
     }
   });
+
+//  var polyline = L.polyline([[48.81, 7.78], [48.46, 7.48]]).addTo(mymap);
+//  mymap.fitBounds(polyline.getBounds());
 }
 
 /*
@@ -84,27 +87,79 @@ function marker(data){
   marker.bindPopup('<p>'+data.adminArea5+'<br/>'+data.latLng.lat+', '+data.latLng.lng+'</p>').openPopup();
 }
 
+/*
+**  Routing AJAX function
+*/
+
 function routing(start, end){
-  console.log(start);
-  console.log(end);
   var dataSend = {
     "locations": [
       start,
       end
     ],
     "options": {
-        "locale": "fr_FR",
-        "unit": "k",
+      "locale": "fr_FR",
+      "unit": "k",
     }
   };
-  console.log(dataSend);
   $.ajax({
     type: 'POST',
     url: 'http://www.mapquestapi.com/directions/v2/route?key=' + key,
-    data: JSON.stringify( dataSend ),
+    data: JSON.stringify(dataSend),
     contentType: "application/json",
     dataType: 'json'
   }).done(function(data) {
-    console.log(data);
+    shape(data.route.sessionId);
+    flag(data.route.locations[0].latLng, data.route.locations[1].latLng);
+    narrative(data.route.legs[0].maneuvers);
   });
+}
+
+/*
+**  Trace the route on the map
+*/
+
+function shape(sessionId){
+  var latlngs = [];
+  var lng;
+  var lat;
+  var i=0;
+  $.get('http://www.mapquestapi.com/directions/v2/routeshape?key='+key+'&sessionId='+sessionId+'&fullShape=true', function(data){
+    for (point of data.route.shape.shapePoints) {
+      if (i == 0){
+        lat = point;
+        i=1;
+      }
+      else {
+        lng = point;
+        latlngs.push([lat, lng]);
+        i=0;
+      }
+    }
+    var polyline = L.polyline(latlngs).addTo(mymap);
+    mymap.fitBounds(polyline.getBounds());
+  });
+}
+
+/*
+**  Place the start and end marker of the route
+*/
+
+function flag(start, end){
+  var marker = L.marker(start).addTo(mymap);
+  marker.bindPopup('<p>Start</p>').openPopup();
+  var marker = L.marker(end).addTo(mymap);
+  marker.bindPopup('<p>End</p>').openPopup();
+}
+
+/*
+**  routing with narrative method
+*/
+
+function narrative(maneuvers){
+  console.log(maneuvers);
+  for (maneuver of maneuvers) {
+    console.log(maneuver);
+    $(".narrative").append('<p>'+maneuver.narrative+'<p>');
+  }
 }
